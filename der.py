366ddab7d18b2ed1f14b67a8377859aa497df23c
Giacomo Cignoni <giacomo.cignoni@gmail.com>
2023-07-03 03:27:28 +0200

    fixed reservoir buffer, added exploratory trials

--- der.py
+++ der.py
@@ -34,15 +34,18 @@ class ReservoirBuffer:
         else:
             # Replace samples with probability buffer_size/stored_samples
             for i in range(batch_size):
-                replace_index = random.randint(0, self.stored_samples)
+                replace_index = random.randint(0, self.stored_samples + i)
+
                 if replace_index < self.buffer_size:
                     self.buffer[replace_index] = (batch_x[i], batch_y[i], batch_logits[i])
+            
+            self.stored_samples += batch_size
 
     def sample(self, batch_size):
         assert batch_size <= self.stored_samples
 
         # Sample batch_size indices
-        indices = random.sample(range(self.stored_samples), batch_size)
+        indices = random.sample(range(len(self.buffer)), batch_size)
         samples = [self.buffer[i] for i in indices]
 
         # Stack each element of the tuples, each sample is a tuple of (x, y, logits)
@@ -53,77 +56,71 @@ class ReservoirBuffer:
         return batch_x, batch_y, batch_logits
 
 
+
 # DER plugin class
 class DerPlugin(SupervisedPlugin):
-    def __init__(self, mem_size=200, replay_mb_size=32, alpha=0.5, beta=0.5):
+    def __init__(self, mem_size=200, alpha=0.5, beta=0.5):
         super().__init__()
         self.buffer = ReservoirBuffer(mem_size)
         self.alpha = alpha
         self.beta = beta
-        self.replay_mb_size = replay_mb_size
 
         if beta == 0:
             self.use_der_plus = False
         else:
             self.use_der_plus = True
 
-    def before_training_exp(self, strategy: "SupervisedTemplate", **kwargs):
-        if len(self.buffer.buffer) == 0:
-            # First experience. We don't use the buffer.
-            self.first_experience = True
+    def before_training_iteration(self, strategy: "SupervisedTemplate", **kwargs):
+        self.batch_size = strategy.mb_x.size(0)
+
+        if len(self.buffer.buffer) > self.batch_size:
+            self.use_buffer = True
             return
         else:
-            self.first_experience = False
-        
+            # Not enough samples stored. We don't use the buffer.
+            self.use_buffer = False
+     
     def before_forward(self, strategy: "SupervisedTemplate", **kwargs):
-        if self.first_experience:
-            return
+        if self.use_buffer:
 
-        # Extract first batch for logits replay from old experiences
-        self.repl_batch_x, _, self.repl_batch_logits = self.buffer.sample(self.replay_mb_size)
+            # Extract first batch for logits replay from old experiences
+            self.repl_batch_x, _, self.repl_batch_logits = self.buffer.sample(self.batch_size)
 
-        if self.use_der_plus:
-            # Extract second batch for DER++
-            repl2_batch_x, self.repl_batch_y, _ = self.buffer.sample(self.replay_mb_size)
-            self.repl_batch_x = torch.cat((self.repl_batch_x, repl2_batch_x))
+            if self.use_der_plus:
+                # Extract second batch for DER++
+                repl2_batch_x, self.repl_batch_y, _ = self.buffer.sample(self.batch_size)
+                self.repl_batch_x = torch.cat((self.repl_batch_x, repl2_batch_x))
 
-        self.repl_batch_x, self.repl_batch_y, self.repl_batch_logits = (
-            self.repl_batch_x.to(strategy.device).detach(),
-            self.repl_batch_y.to(strategy.device).detach(),
-            self.repl_batch_logits.to(strategy.device).detach(),
-        )
+            self.repl_batch_x, self.repl_batch_y, self.repl_batch_logits = (
+                self.repl_batch_x.to(strategy.device).detach(),
+                self.repl_batch_y.to(strategy.device).detach(),
+                self.repl_batch_logits.to(strategy.device).detach(),
+            )
 
-        # Forward on the replay batch
-        self.repl_output = strategy.model(self.repl_batch_x).detach()
+            # Forward on the replay batch
+            self.repl_output = strategy.model(self.repl_batch_x).detach()
 
     def before_backward(self, strategy: "SupervisedTemplate", **kwargs):
-        if self.first_experience:
-            return
-        # Compute the DER loss on the replay batch
-        der_loss = self.alpha * F.mse_loss(self.repl_output[:self.replay_mb_size], self.repl_batch_logits)
-        if self.use_der_plus:
-            # Add the DER++ loss
-            der_loss += self.beta* F.cross_entropy(self.repl_output[self.replay_mb_size:], self.repl_batch_y)
+        if self.use_buffer:
 
-        strategy.loss += der_loss
+            # Compute the DER loss on the replay batch
+            der_loss = self.alpha * F.mse_loss(self.repl_output[:self.batch_size], self.repl_batch_logits)
+            if self.use_der_plus:
+                # Add the DER++ loss
+                der_loss += self.beta* F.cross_entropy(self.repl_output[self.batch_size:], self.repl_batch_y)
+
+            strategy.loss += der_loss
 
     def after_training_iteration(self, strategy: "SupervisedTemplate", **kwargs):
         # Add the current batch to the buffer
        self.buffer.add(strategy.mb_x.detach(), strategy.mb_y.detach(), strategy.mb_output.detach())
 
 
-    # def after_training_exp(self, strategy: "SupervisedTemplate", **kwargs):
-
-    #     print('Memory usage: ', torch.cuda.memory_allocated()/torch.cuda.max_memory_allocated()*100, '%')
-
-    #     # print the number of unique labels in the buffer
-    #     print('BUFFER CLASSES INFO')
-    #     labels_list = []
-    #     for sample in self.buffer.buffer:
-    #         labels_list.append(sample[1].cpu().numpy())
-    #     # Get unique elements and their counts
-    #     unique_elements, counts = np.unique(labels_list, return_counts=True)
-
-    #     # Print unique elements and their counts
-    #     for element, count in zip(unique_elements, counts):
-    #         print(f"Element: {element}, Count: {count}")
+    # Get the unique labels and their counts in the buffer
+    def get_buffer_labels(self):
+        labels_list = []
+        for sample in self.buffer.buffer:
+            labels_list.append(sample[1].cpu().numpy())
+        # Get unique elements and their counts
+        unique_elements, counts = np.unique(labels_list, return_counts=True)
+        return unique_elements, counts
